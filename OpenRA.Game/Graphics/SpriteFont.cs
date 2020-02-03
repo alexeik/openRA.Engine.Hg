b87a91b1e25dad02cd1646a512673fa47827d383
alexeik <transmissive.trash@gmail.com>
2020-02-04 00:04:45 +0300

    cosmetic

--- OpenRA.Game/Graphics/SpriteFont.cs
+++ OpenRA.Game/Graphics/SpriteFont.cs
@@ -1,245 +1,245 @@
-#region Copyright & License Information
-/*
- * Copyright 2007-2019 The OpenRA Developers (see AUTHORS)
- * This file is part of OpenRA, which is free software. It is made
- * available to you under the terms of the GNU General Public License
- * as published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version. For more
- * information, see COPYING.
- */
-#endregion
+//#region Copyright & License Information
+///*
+// * Copyright 2007-2019 The OpenRA Developers (see AUTHORS)
+// * This file is part of OpenRA, which is free software. It is made
+// * available to you under the terms of the GNU General Public License
+// * as published by the Free Software Foundation, either version 3 of
+// * the License, or (at your option) any later version. For more
+// * information, see COPYING.
+// */
+//#endregion
 
-using System;
-using System.Linq;
-using OpenRA.Primitives;
-using OpenRA.Support;
-using OpenRA.Widgets;
+//using System;
+//using System.Linq;
+//using OpenRA.Primitives;
+//using OpenRA.Support;
+//using OpenRA.Widgets;
 
-namespace OpenRA.Graphics
-{
-	public sealed class SpriteFont : IDisposable
-	{
-		public int TopOffset { get; private set; }
-		readonly int size;
-		readonly SheetBuilder builder;
-		readonly Func<string, float> lineWidth;
-		readonly IFont font;
-		readonly Cache<Pair<char, Color>, GlyphInfo> glyphs;
+//namespace OpenRA.Graphics
+//{
+//	public sealed class SpriteFont : IDisposable
+//	{
+//		public int TopOffset { get; private set; }
+//		readonly int size;
+//		readonly SheetBuilder builder;
+//		readonly Func<string, float> lineWidth;
+//		readonly IFont font;
+//		readonly Cache<Pair<char, Color>, GlyphInfo> glyphs;
 
-		float deviceScale;
+//		float deviceScale;
 
-		public SpriteFont(string name, byte[] data, int size, float scale, SheetBuilder builder)
-		{
-			if (builder.Type != SheetType.BGRA)
-				throw new ArgumentException("The sheet builder must create BGRA sheets.", "builder");
+//		public SpriteFont(string name, byte[] data, int size, float scale, SheetBuilder builder)
+//		{
+//			if (builder.Type != SheetType.BGRA)
+//				throw new ArgumentException("The sheet builder must create BGRA sheets.", "builder");
 
-			deviceScale = scale;
-			this.size = size;
-			this.builder = builder;
+//			deviceScale = scale;
+//			this.size = size;
+//			this.builder = builder;
 
-			font = Game.Renderer.CreateFont(data); // FreeTypeFont библиотека создает IFont структуру, где есть байтовое представление символа.
-			font.SetSize(size, deviceScale);
+//			font = Game.Renderer.CreateFont(data); // FreeTypeFont библиотека создает IFont структуру, где есть байтовое представление символа.
+//			font.SetSize(size, deviceScale);
 
-			glyphs = new Cache<Pair<char, Color>, GlyphInfo>(CreateGlyph, Pair<char, Color>.EqualityComparer);
+//			glyphs = new Cache<Pair<char, Color>, GlyphInfo>(CreateGlyph, Pair<char, Color>.EqualityComparer);
 
-			// PERF: Cache these delegates for Measure calls.
-			Func<char, float> characterWidth = character => glyphs[Pair.New(character, Color.White)].Advance; // это одна функция с аргументом character, а телом из glyphs[Pair.New(character, Color.White)].Advance
+//			// PERF: Cache these delegates for Measure calls.
+//			Func<char, float> characterWidth = character => glyphs[Pair.New(character, Color.White)].Advance; // это одна функция с аргументом character, а телом из glyphs[Pair.New(character, Color.White)].Advance
 
-			lineWidth = line => line.Sum(characterWidth) / deviceScale; // тоже функция как и characterWidth
+//			lineWidth = line => line.Sum(characterWidth) / deviceScale; // тоже функция как и characterWidth
 
-			if (size <= 24) // пытается, подобрать size? чтобы в строку влезло 24 символа.
-				PrecacheColor(Color.White, name);
+//			if (size <= 24) // пытается, подобрать size? чтобы в строку влезло 24 символа.
+//				PrecacheColor(Color.White, name);
 
-			TopOffset = size - font.Height;
-		}
+//			TopOffset = size - font.Height;
+//		}
 
-		public void SetScale(float scale)
-		{
-			deviceScale = scale;
+//		public void SetScale(float scale)
+//		{
+//			deviceScale = scale;
 
-			font.SetSize(size, scale);
-			glyphs.Clear();
+//			font.SetSize(size, scale);
+//			glyphs.Clear();
 
-			TopOffset = size - font.Height;
-		}
+//			TopOffset = size - font.Height;
+//		}
 
-		/// <summary>
-		/// Этим методом генерируется обращение к каждой букве в шрифте, что заставляет SpriteFont сгенерировать картинки букв.
-		/// </summary>
-		/// <param name="c">буква.</param>
-		/// <param name="name">название шрифта.</param>
-		void PrecacheColor(Color c, string name)
-		{
-			using (new PerfTimer("PrecacheColor {0} {1}px {2}".F(name, size, c)))
+//		/// <summary>
+//		/// Этим методом генерируется обращение к каждой букве в шрифте, что заставляет SpriteFont сгенерировать картинки букв.
+//		/// </summary>
+//		/// <param name="c">буква.</param>
+//		/// <param name="name">название шрифта.</param>
+//		void PrecacheColor(Color c, string name)
+//		{
+//			using (new PerfTimer("PrecacheColor {0} {1}px {2}".F(name, size, c)))
 
-				for (var n = (char)0x20; n < (char)0x7f; n++)
-				{
-					if (glyphs[Pair.New(n, c)] == null)
-					{
-						throw new InvalidOperationException();
-					}
-				}
-		}
+//				for (var n = (char)0x20; n < (char)0x7f; n++)
+//				{
+//					if (glyphs[Pair.New(n, c)] == null)
+//					{
+//						throw new InvalidOperationException();
+//					}
+//				}
+//		}
 
-		public void DrawText(string text, float2 location, Color c)
-		{
-			// Offset from the baseline position to the top-left of the glyph for rendering
-			location += new float2(0, size);
+//		public void DrawText(string text, float2 location, Color c)
+//		{
+//			// Offset from the baseline position to the top-left of the glyph for rendering
+//			location += new float2(0, size);
 
-			var p = location;
-			foreach (var s in text)
-			{
-				if (s == '\n')
-				{
-					location += new float2(0, size);
-					p = location;
-					continue;
-				}
+//			var p = location;
+//			foreach (var s in text)
+//			{
+//				if (s == '\n')
+//				{
+//					location += new float2(0, size);
+//					p = location;
+//					continue;
+//				}
 
-				var g = glyphs[Pair.New(s, c)];
+//				var g = glyphs[Pair.New(s, c)];
 
-				float3 tempXY = new float2((int)Math.Round(p.X * deviceScale + g.Offset.X, 0) / deviceScale, p.Y + g.Offset.Y / deviceScale);
+//				float3 tempXY = new float2((int)Math.Round(p.X * deviceScale + g.Offset.X, 0) / deviceScale, p.Y + g.Offset.Y / deviceScale);
 
-				if (g.Sprite != null)
-				{
-					g.Sprite.SpriteType = 1; // 1 будет для FontMSDF
-					g.Sprite.SpriteArrayNum = (int)s;
+//				if (g.Sprite != null)
+//				{
+//					g.Sprite.SpriteType = 1; // 1 будет для FontMSDF
+//					g.Sprite.SpriteArrayNum = (int)s;
 
-					if ((int)s > 90 || (int)s < 65)
-					{
-						g.Sprite.Top = 0.8406f;
-						g.Sprite.Left = 0;
-						g.Sprite.Bottom = 0;
-						g.Sprite.Right = 0.6531f;
-					}
-					if ((int)s >= 65 && (int)s <= 90)
-					{
-						g.Sprite.Top = 0.9844f;
-						g.Sprite.Left = 0;
-						g.Sprite.Bottom = 0;
-						g.Sprite.Right = 0.7813f;
-					}
-					Game.Renderer.FontSpriteRenderer.DrawSprite(g.Sprite, tempXY, 0, g.Sprite.Size / deviceScale);
+//					if ((int)s > 90 || (int)s < 65)
+//					{
+//						g.Sprite.Top = 0.8406f;
+//						g.Sprite.Left = 0;
+//						g.Sprite.Bottom = 0;
+//						g.Sprite.Right = 0.6531f;
+//					}
+//					if ((int)s >= 65 && (int)s <= 90)
+//					{
+//						g.Sprite.Top = 0.9844f;
+//						g.Sprite.Left = 0;
+//						g.Sprite.Bottom = 0;
+//						g.Sprite.Right = 0.7813f;
+//					}
+//					Game.Renderer.FontSpriteRenderer.DrawSprite(g.Sprite, tempXY, 0, g.Sprite.Size / deviceScale);
 
-					// Game.Renderer.RgbaSpriteRenderer.DrawSprite(g.Sprite, tempXY, g.Sprite.Size / deviceScale);
-				}
+//					// Game.Renderer.RgbaSpriteRenderer.DrawSprite(g.Sprite, tempXY, g.Sprite.Size / deviceScale);
+//				}
 
-				p += new float2(g.Advance / deviceScale, 0);
-			}
-			Game.Renderer.FontSpriteRenderer.SetTextColor(c);
+//				p += new float2(g.Advance / deviceScale, 0);
+//			}
+//			Game.Renderer.FontSpriteRenderer.SetTextColor(c);
 
-			// + добавить юсда передачу параметра в шейдер , цвет шрифта.
-		}
+//			// + добавить юсда передачу параметра в шейдер , цвет шрифта.
+//		}
 
-		public void DrawTextWithContrast(string text, float2 location, Color fg, Color bg, int offset)
-		{
-			if (offset > 0)
-			{
-				DrawText(text, location + new float2(-offset / deviceScale, 0), bg);
-				DrawText(text, location + new float2(offset / deviceScale, 0), bg);
-				DrawText(text, location + new float2(0, -offset / deviceScale), bg);
-				DrawText(text, location + new float2(0, offset / deviceScale), bg);
-			}
+//		public void DrawTextWithContrast(string text, float2 location, Color fg, Color bg, int offset)
+//		{
+//			if (offset > 0)
+//			{
+//				DrawText(text, location + new float2(-offset / deviceScale, 0), bg);
+//				DrawText(text, location + new float2(offset / deviceScale, 0), bg);
+//				DrawText(text, location + new float2(0, -offset / deviceScale), bg);
+//				DrawText(text, location + new float2(0, offset / deviceScale), bg);
+//			}
 
-			DrawText(text, location, fg);
-		}
+//			DrawText(text, location, fg);
+//		}
 
-		public void DrawTextWithContrast(string text, float2 location, Color fg, Color bgDark, Color bgLight, int offset)
-		{
-			DrawTextWithContrast(text, location, fg, GetContrastColor(fg, bgDark, bgLight), offset);
-		}
+//		public void DrawTextWithContrast(string text, float2 location, Color fg, Color bgDark, Color bgLight, int offset)
+//		{
+//			DrawTextWithContrast(text, location, fg, GetContrastColor(fg, bgDark, bgLight), offset);
+//		}
 
-		public void DrawTextWithShadow(string text, float2 location, Color fg, Color bg, int offset)
-		{
-			if (offset != 0)
-				DrawText(text, location + new float2(offset, offset), bg);
+//		public void DrawTextWithShadow(string text, float2 location, Color fg, Color bg, int offset)
+//		{
+//			if (offset != 0)
+//				DrawText(text, location + new float2(offset, offset), bg);
 
-			DrawText(text, location, fg);
-		}
+//			DrawText(text, location, fg);
+//		}
 
-		public void DrawTextWithShadow(string text, float2 location, Color fg, Color bgDark, Color bgLight, int offset)
-		{
-			DrawTextWithShadow(text, location, fg, GetContrastColor(fg, bgDark, bgLight), offset);
-		}
+//		public void DrawTextWithShadow(string text, float2 location, Color fg, Color bgDark, Color bgLight, int offset)
+//		{
+//			DrawTextWithShadow(text, location, fg, GetContrastColor(fg, bgDark, bgLight), offset);
+//		}
 
-		public int2 Measure(string text)
-		{
-			if (string.IsNullOrEmpty(text))
-				return new int2(0, size);
+//		public int2 Measure(string text)
+//		{
+//			if (string.IsNullOrEmpty(text))
+//				return new int2(0, size);
 
-			var lines = text.Split('\n');
-			return new int2((int)Math.Ceiling(lines.Max(lineWidth)), lines.Length * size);
-		}
+//			var lines = text.Split('\n');
+//			return new int2((int)Math.Ceiling(lines.Max(lineWidth)), lines.Length * size);
+//		}
 
-		/// <summary>
-		/// Запускается из PrecacheColor.
-		/// </summary>
-		/// <param name="c">Буква шрифта.</param>
-		/// <returns>Один визуальный образ буквы.</returns>
-		GlyphInfo CreateGlyph(Pair<char, Color> c)
-		{
-			var glyph = font.CreateGlyph(c.First);
+//		/// <summary>
+//		/// Запускается из PrecacheColor.
+//		/// </summary>
+//		/// <param name="c">Буква шрифта.</param>
+//		/// <returns>Один визуальный образ буквы.</returns>
+//		GlyphInfo CreateGlyph(Pair<char, Color> c)
+//		{
+//			var glyph = font.CreateGlyph(c.First);
 
-			if (glyph.Data == null)
-			{
-				return new GlyphInfo
-				{
-					Sprite = null,
-					Advance = 0,
-					Offset = int2.Zero
-				};
-			}
+//			if (glyph.Data == null)
+//			{
+//				return new GlyphInfo
+//				{
+//					Sprite = null,
+//					Advance = 0,
+//					Offset = int2.Zero
+//				};
+//			}
 
-			var s = builder.Allocate(glyph.Size);
-			var g = new GlyphInfo
-			{
-				Sprite = s,
-				Advance = glyph.Advance,
-				Offset = glyph.Offset
-			};
+//			var s = builder.Allocate(glyph.Size);
+//			var g = new GlyphInfo
+//			{
+//				Sprite = s,
+//				Advance = glyph.Advance,
+//				Offset = glyph.Offset
+//			};
 
-			var dest = s.Sheet.GetData();
-			var destStride = s.Sheet.Size.Width * 4;
+//			var dest = s.Sheet.GetData();
+//			var destStride = s.Sheet.Size.Width * 4;
 
-			for (var j = 0; j < s.Size.Y; j++)
-			{
-				for (var i = 0; i < s.Size.X; i++)
-				{
-					// тут происходит копирование байтов из глифа в "p", а после в dest общий массив текстуры.
-					var p = glyph.Data[j * glyph.Size.Width + i];
-					if (p != 0)
-					{
-						var q = destStride * (j + s.Bounds.Top) + 4 * (i + s.Bounds.Left);
-						var pmc = Util.PremultiplyAlpha(Color.FromArgb(p, c.Second));
+//			for (var j = 0; j < s.Size.Y; j++)
+//			{
+//				for (var i = 0; i < s.Size.X; i++)
+//				{
+//					// тут происходит копирование байтов из глифа в "p", а после в dest общий массив текстуры.
+//					var p = glyph.Data[j * glyph.Size.Width + i];
+//					if (p != 0)
+//					{
+//						var q = destStride * (j + s.Bounds.Top) + 4 * (i + s.Bounds.Left);
+//						var pmc = Util.PremultiplyAlpha(Color.FromArgb(p, c.Second));
 
-						dest[q] = pmc.B;
-						dest[q + 1] = pmc.G;
-						dest[q + 2] = pmc.R;
-						dest[q + 3] = pmc.A;
-					}
-				}
-			}
+//						dest[q] = pmc.B;
+//						dest[q + 1] = pmc.G;
+//						dest[q + 2] = pmc.R;
+//						dest[q + 3] = pmc.A;
+//					}
+//				}
+//			}
 
-			s.Sheet.CommitBufferedData();
+//			s.Sheet.CommitBufferedData();
 
-			return g;
-		}
+//			return g;
+//		}
 
-		static Color GetContrastColor(Color fgColor, Color bgDark, Color bgLight)
-		{
-			return fgColor == Color.White || fgColor.GetBrightness() > 0.33 ? bgDark : bgLight;
-		}
+//		static Color GetContrastColor(Color fgColor, Color bgDark, Color bgLight)
+//		{
+//			return fgColor == Color.White || fgColor.GetBrightness() > 0.33 ? bgDark : bgLight;
+//		}
 
-		public void Dispose()
-		{
-			font.Dispose();
-		}
-	}
+//		public void Dispose()
+//		{
+//			font.Dispose();
+//		}
+//	}
 
-	class GlyphInfo
-	{
-		public float Advance;
-		public int2 Offset;
-		public Sprite Sprite;
-	}
-}
+//	class GlyphInfo2
+//	{
+//		public float Advance;
+//		public int2 Offset;
+//		public Sprite Sprite;
+//	}
+//}
