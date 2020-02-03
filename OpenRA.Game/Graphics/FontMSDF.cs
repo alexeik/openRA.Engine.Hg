3fdf8f3fb1fe0ab8755c92545ca1d001c31af97d
alexeik <transmissive.trash@gmail.com>
2020-02-03 15:59:42 +0300

    font png sprites loader for TextureArray

--- OpenRA.Game/Graphics/FontMSDF.cs
+++ OpenRA.Game/Graphics/FontMSDF.cs
@@ -11,15 +11,14 @@ namespace OpenRA.Graphics
 	public class FontMSDF
 	{
 		public ITexture Texture { get; private set; }
-		public int Size = 64;
+		public static int Size = 64;
 		public FontMSDF()
 		{
 			Texture = Game.Renderer.Context.CreateTexture2DArray();
 			Texture.SetEmpty(Size, Size, 127);// просто заготовка gjl 127 слойный массив, каждый слой 96 на 96 пикселей.
 		}
-		public void LoadFontTextures()
+		public void LoadFontTexturesAsPng()
 		{
-			
 			FileSystem.IReadOnlyPackage pack;
 			string temp;
 			if (Game.ModData.DefaultFileSystem.TryGetPackageContaining(@"120.png", out pack, out temp))
@@ -36,6 +35,33 @@ namespace OpenRA.Graphics
 						}
 						catch (Exception e)
 						{
+							Console.WriteLine("Error loading char: {0} x {1}", f, Convert.ToInt32(f.Split('.')[0]));
+						}
+					}
+				}
+			}
+		}
+		public void LoadFontTextures()
+		{
+			FileSystem.IReadOnlyPackage pack;
+			string temp;
+			if (Game.ModData.DefaultFileSystem.TryGetPackageContaining(@"120.bin", out pack, out temp))
+			{
+				foreach (string f in pack.Contents)
+				{
+					using (var stream = pack.GetStream(f))
+					{
+						using (MemoryStream ms = new MemoryStream())
+						{
+							try
+							{
+								stream.CopyTo(ms);
+								Texture.SetData(ms.ToArray(), Size, Size, Convert.ToInt32(f.Split('.')[0]));
+							}
+							catch (Exception e)
+							{
+								Console.WriteLine("Error loading char: {0} x {1}", f, Convert.ToInt32(f.Split('.')[0]));
+							}
 						}
 					}
 				}
